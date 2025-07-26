aa87641ac5002c19b8753619f809f26de1fca0e5
Lekuru <contact@lekuru.xyz>
2025-07-26 22:09:04 +0200

    Add tests for other 3 banners

--- tests/renderer_test.go
+++ tests/renderer_test.go
@@ -10,9 +10,9 @@ import (
 	"github.com/Lekuruu/osutp-web/pkg/banners"
 )
 
-func TestBannerRender(t *testing.T) {
+func TestBannerRenderRegular(t *testing.T) {
 	player := banners.NewPlayer("Cookiezi", "Korea", "KR", "../web/static/images/flags/kr.gif", 1, 1, 3790)
-	banner := banners.NewCleanStyleCenteredBanner(player)
+	banner := banners.NewCleanStyleBanner(player)
 	if banner == nil {
 		t.Fatal("Failed to create CleanStyleBanner")
 	}
@@ -22,7 +22,55 @@ func TestBannerRender(t *testing.T) {
 		t.Error("Render() returned nil image")
 	}
 
-	outFile, err := os.Create("test_banner.png")
+	outFile, err := os.Create("test_banner_cs.png")
+	if err != nil {
+		t.Fatalf("Failed to create output file: %v", err)
+	}
+	defer outFile.Close()
+
+	err = png.Encode(outFile, img)
+	if err != nil {
+		t.Fatalf("Failed to encode image: %v", err)
+	}
+}
+
+func TestBannerRenderCentered(t *testing.T) {
+	player := banners.NewPlayer("Cookiezi", "Korea", "KR", "../web/static/images/flags/kr.gif", 1, 1, 3790)
+	banner := banners.NewCleanStyleCenteredBanner(player)
+	if banner == nil {
+		t.Fatal("Failed to create CleanStyleCenteredBanner")
+	}
+
+	img := banner.Render()
+	if img == nil {
+		t.Error("Render() returned nil image")
+	}
+
+	outFile, err := os.Create("test_banner_csc.png")
+	if err != nil {
+		t.Fatalf("Failed to create output file: %v", err)
+	}
+	defer outFile.Close()
+
+	err = png.Encode(outFile, img)
+	if err != nil {
+		t.Fatalf("Failed to encode image: %v", err)
+	}
+}
+
+func TestBannerRenderOneLine(t *testing.T) {
+	player := banners.NewPlayer("Cookiezi", "Korea", "KR", "../web/static/images/flags/kr.gif", 1, 1, 3790)
+	banner := banners.NewCleanStyleOneLineBanner(player)
+	if banner == nil {
+		t.Fatal("Failed to create CleanStyleOneLineBanner")
+	}
+
+	img := banner.Render()
+	if img == nil {
+		t.Error("Render() returned nil image")
+	}
+
+	outFile, err := os.Create("test_banner_cso.png")
 	if err != nil {
 		t.Fatalf("Failed to create output file: %v", err)
 	}
