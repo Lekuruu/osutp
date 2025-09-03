3ddf8b719c313f4fa1400bf4c009cb19a8d78db4
Lekuru <contact@lekuru.xyz>
2025-09-03 22:21:03 +0200

    Move constants to seperate file

--- internal/importers/titanic/beatmaps.go
+++ internal/importers/titanic/beatmaps.go
@@ -12,77 +12,6 @@ import (
 	"github.com/Lekuruu/osutp-web/internal/services"
 )
 
-const (
-	GameModeOsu   = 0
-	GameModeTaiko = 1
-	GameModeCatch = 2
-	GameModeMania = 3
-)
-
-const (
-	BeatmapCategoryAny         = 0
-	BeatmapCategoryLeaderboard = 1
-	BeatmapCategoryRanked      = 2
-	BeatmapCategoryQualified   = 3
-	BeatmapCategoryLoved       = 4
-	BeatmapCategoryApproved    = 5
-	BeatmapCategoryPending     = 6
-	BeatmapCategoryWIP         = 7
-	BeatmapCategoryGraveyard   = 8
-)
-
-const (
-	BeatmapSortByTitle      = 0
-	BeatmapSortByArtist     = 1
-	BeatmapSortByCreator    = 2
-	BeatmapSortByDifficulty = 3
-	BeatmapSortByRanked     = 4
-	BeatmapSortByRating     = 5
-	BeatmapSortByPlays      = 6
-	BeatmapSortByCreated    = 7
-	BeatmapSortByRelevance  = 8
-)
-
-const (
-	BeatmapOrderDescending = 0
-	BeatmapOrderAscending  = 1
-)
-
-const (
-	BeatmapLanguageAny          = 0
-	BeatmapLanguageUnspecified  = 1
-	BeatmapLanguageEnglish      = 2
-	BeatmapLanguageJapanese     = 3
-	BeatmapLanguageChinese      = 4
-	BeatmapLanguageInstrumental = 5
-	BeatmapLanguageKorean       = 6
-	BeatmapLanguageFrench       = 7
-	BeatmapLanguageGerman       = 8
-	BeatmapLanguageSwedish      = 9
-	BeatmapLanguageSpanish      = 10
-	BeatmapLanguageItalian      = 11
-	BeatmapLanguageRussian      = 12
-	BeatmapLanguagePolish       = 13
-	BeatmapLanguageOther        = 14
-)
-
-const (
-	BeatmapGenreAny         = 0
-	BeatmapGenreUnspecified = 1
-	BeatmapGenreVideoGame   = 2
-	BeatmapGenreAnime       = 3
-	BeatmapGenreRock        = 4
-	BeatmapGenrePop         = 5
-	BeatmapGenreOther       = 6
-	BeatmapGenreNovelty     = 7
-	BeatmapGenreHipHop      = 9
-	BeatmapGenreElectronic  = 10
-	BeatmapGenreMetal       = 11
-	BeatmapGenreClassical   = 12
-	BeatmapGenreFolk        = 13
-	BeatmapGenreJazz        = 14
-)
-
 type BeatmapSearchRequest struct {
 	Language   *int    `json:"language,omitempty"`
 	Genre      *int    `json:"genre,omitempty"`
@@ -101,7 +30,6 @@ type BeatmapSearchRequest struct {
 	Page       int     `json:"page"`
 }
 
-// BeatmapsetModel represents a beatmapset with its metadata
 type BeatmapsetModel struct {
 	ID                 int            `json:"id"`
 	Title              *string        `json:"title,omitempty"`
@@ -127,7 +55,6 @@ type BeatmapsetModel struct {
 	Beatmaps           []BeatmapModel `json:"beatmaps"`
 }
 
-// BeatmapModel represents an individual beatmap inside a set
 type BeatmapModel struct {
 	ID           int     `json:"id"`
 	SetID        int     `json:"set_id"`
---
+++ internal/importers/titanic/constants.go
@@ -0,0 +1,72 @@
+package titanic
+
+const (
+	GameModeOsu   = 0
+	GameModeTaiko = 1
+	GameModeCatch = 2
+	GameModeMania = 3
+)
+
+const (
+	BeatmapCategoryAny         = 0
+	BeatmapCategoryLeaderboard = 1
+	BeatmapCategoryRanked      = 2
+	BeatmapCategoryQualified   = 3
+	BeatmapCategoryLoved       = 4
+	BeatmapCategoryApproved    = 5
+	BeatmapCategoryPending     = 6
+	BeatmapCategoryWIP         = 7
+	BeatmapCategoryGraveyard   = 8
+)
+
+const (
+	BeatmapSortByTitle      = 0
+	BeatmapSortByArtist     = 1
+	BeatmapSortByCreator    = 2
+	BeatmapSortByDifficulty = 3
+	BeatmapSortByRanked     = 4
+	BeatmapSortByRating     = 5
+	BeatmapSortByPlays      = 6
+	BeatmapSortByCreated    = 7
+	BeatmapSortByRelevance  = 8
+)
+
+const (
+	BeatmapOrderDescending = 0
+	BeatmapOrderAscending  = 1
+)
+
+const (
+	BeatmapLanguageAny          = 0
+	BeatmapLanguageUnspecified  = 1
+	BeatmapLanguageEnglish      = 2
+	BeatmapLanguageJapanese     = 3
+	BeatmapLanguageChinese      = 4
+	BeatmapLanguageInstrumental = 5
+	BeatmapLanguageKorean       = 6
+	BeatmapLanguageFrench       = 7
+	BeatmapLanguageGerman       = 8
+	BeatmapLanguageSwedish      = 9
+	BeatmapLanguageSpanish      = 10
+	BeatmapLanguageItalian      = 11
+	BeatmapLanguageRussian      = 12
+	BeatmapLanguagePolish       = 13
+	BeatmapLanguageOther        = 14
+)
+
+const (
+	BeatmapGenreAny         = 0
+	BeatmapGenreUnspecified = 1
+	BeatmapGenreVideoGame   = 2
+	BeatmapGenreAnime       = 3
+	BeatmapGenreRock        = 4
+	BeatmapGenrePop         = 5
+	BeatmapGenreOther       = 6
+	BeatmapGenreNovelty     = 7
+	BeatmapGenreHipHop      = 9
+	BeatmapGenreElectronic  = 10
+	BeatmapGenreMetal       = 11
+	BeatmapGenreClassical   = 12
+	BeatmapGenreFolk        = 13
+	BeatmapGenreJazz        = 14
+)
