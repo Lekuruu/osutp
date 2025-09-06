3a64f1bba9dad9fb11b95720dcb2e1994e7c4620
Lekuru <contact@lekuru.xyz>
2025-09-06 21:37:33 +0200

    Move freedom dive straingraph to static

--- cmd/website/main.go
+++ cmd/website/main.go
@@ -18,11 +18,13 @@ func InitializeRoutes(server *common.Server) {
 	server.Router.HandleFunc("/changelog", server.ContextMiddleware(routes.Changelog)).Methods("GET")
 	server.Router.HandleFunc("/banners/{pid:[0-9]+}", server.ContextMiddleware(routes.Banners)).Methods("GET")
 	server.Router.HandleFunc("/banner/{pid:[0-9]+}", server.ContextMiddleware(routes.BannerImage)).Methods("GET")
+	// TODO: Implement dynamic strain graphs
 
 	// Initialize static routes
 	server.Router.PathPrefix("/js/").Handler(http.StripPrefix("/js/", http.FileServer(http.Dir("web/static/js/"))))
 	server.Router.PathPrefix("/css/").Handler(http.StripPrefix("/css/", http.FileServer(http.Dir("web/static/css/"))))
 	server.Router.PathPrefix("/images/").Handler(http.StripPrefix("/images/", http.FileServer(http.Dir("web/static/images/"))))
+	server.Router.PathPrefix("/straingraph/").Handler(http.StripPrefix("/straingraph/", http.FileServer(http.Dir("web/static/straingraph/"))))
 	server.Router.HandleFunc("/robots.txt", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "web/static/robots.txt")
 	})
